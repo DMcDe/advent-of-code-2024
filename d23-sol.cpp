49165def0b427cbead4b72118235116b48ae5d9a
DMcDe <dmcde@umich.edu>
2024-12-23 16:22:44 -0600

    Update day 23

--- d23-sol.cpp
+++ d23-sol.cpp
@@ -1,9 +1,13 @@
 using namespace std;
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <set>
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -26,12 +30,13 @@ int main(int argc, char* argv[]) {
     int tTriples = 0;
 
     for (auto it = mp.begin(); it != mp.end(); it++) {
+        // Loop through nodes starting w/ 'T'
         if (it->first[0] == 't') {
             for (auto i = it->second.begin(); i != it->second.end(); i++) {
-                if ((*i)[0] == 't' && (*i) < it->first) continue;
+                if ((*i)[0] == 't' && (*i) < it->first) continue; // Prevent double counting w/ multiple Ts
                 for (auto j = i; j != it->second.end(); j++) {
                     if (mp[*i].count(*j)) {
-                        if ((*j)[0] == 't' && (*j) < it->first) continue;
+                        if ((*j)[0] == 't' && (*j) < it->first) continue; // Prevent double counting w/ multiple Ts
                         cout << "Adding triplet: " << it->first << ", " << *i << ", " << *j << "\n";
                         tTriples++;
                     }
@@ -42,6 +47,55 @@ int main(int argc, char* argv[]) {
 
     cout << "The number of triplets with T computers is: " << tTriples << "\n";
 
+    // Track which nodes have been seen
+    unordered_set<string> seen;
 
+    // Keep a list of all groups
+    vector<unordered_set<string>> groups;
+
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        if (!seen.count(it->first)) {
+            // If haven't visited already, investigate
+
+            // Track members of current network
+            unordered_set<string> group;
+
+            // Store connected nodes in a queue
+            deque<string> neighbors(it->second.begin(), it->second.end());
+
+            while (!neighbors.empty()) {
+                string n = neighbors.front();
+                neighbors.pop_front();
+
+                // Skip over if already visited
+                if (!seen.count(n)) {
+                    seen.insert(n);
+                    group.insert(n);
+
+                    // Add neighbors of neighbor to queue
+                    for (auto i = mp[n].begin(); i != mp[n].end(); i++) neighbors.push_back(*i);
+                }
+            }
+
+            groups.push_back(group);
+        }
+    }
+
+    int bestSize = groups[0].size();
+    int bestI = 0;
+
+    for (int i = 1; i < groups.size(); i++) {
+        if (groups[i].size() > bestSize) {
+            bestSize = groups[i].size();
+            bestI = i;
+        }
+    }
+
+    sort(groups[bestI].begin(), groups[bestI].end());
+
+    cout << "Best group:\n";
+
+    for (string m : groups[bestI]) cout << m << ",";
+    
     return 0;
 }
